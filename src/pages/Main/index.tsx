e67ce851c08785b6792a87f4c6f52fbc55b2f805
farbod <farboddeylami@gmail.com>
2022-02-10 23:53:15 +0330

    fix create db bug (temporary)

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { openDB } from 'idb';
+import { useAppSelector, useAppDispatch } from '../../hooks';
+import { initial } from './slice';
 
 import styles from './index.module.scss';
 
 const Main = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initial({ openDB }));
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.main}>
--- src/pages/Main/slice.ts
+++ src/pages/Main/slice.ts
@@ -31,62 +31,28 @@ export const initial = createAsyncThunk(
   'main/initial',
   async ({ openDB }: { openDB: any }) => {
     try {
-      const storeName = 'list';
-      const db = await openDB('online-player', 1);
-      // const items = await db.getAll(storeName);
-      const items = [];
-      const keys = await db.getAllKeys(storeName);
-      for (let i = 0; i < keys.length; i++) {
-        const item = await db.get(storeName, keys[i]);
-        items.push(item);
-        items[i].key = keys[i];
-      }
+      const db = await openDB('online-player', 1, {
+        upgrade(db: any, oldVersion: any, newVersion: any, transaction: any) {
+          const handlesStore = db.createObjectStore('handles', {
+            autoIncrement: false,
+          });
+          const listStore = db.createObjectStore('list', {
+            autoIncrement: true,
+          });
+          const artistsStore = db.createObjectStore('artists', {
+            autoIncrement: true,
+          });
+          const albumsStore = db.createObjectStore('albums', {
+            autoIncrement: true,
+          });
+        },
+      });
 
-      return items;
+      return [];
     } catch (err) {
       console.log(err);
       return [];
     }
-  }
-);
-
-export const updateFiles = createAsyncThunk(
-  'main/updateFiles',
-  async ({ openDB, deleteDB }: { openDB: any; deleteDB: any }) => {
-    const directoryHandle = await (window as any).showDirectoryPicker();
-    const files = await getFilesListFromDirectory(directoryHandle, ['mp3']);
-
-    await deleteDB('online-player');
-
-    const db = await openDB('online-player', 1, {
-      upgrade(db: any, oldVersion: any, newVersion: any, transaction: any) {
-        const handlesStore = db.createObjectStore('handles', {
-          autoIncrement: false,
-        });
-        const listStore = db.createObjectStore('list', { autoIncrement: true });
-      },
-    });
-
-    {
-      const storeName = 'handles';
-      const tx = db.transaction(storeName, 'readwrite');
-      const store = await tx.objectStore(storeName);
-      const value = await store.put(directoryHandle, 'rootHandle');
-      await tx.done;
-    }
-
-    {
-      const storeName = 'list';
-      // await db.put(storeName, { otherstuff: '...' }, 'somewhere/file.something');
-      const tx = db.transaction(storeName, 'readwrite');
-      const store = await tx.objectStore(storeName);
-      for (let i = 0; i < files.length; i++) {
-        const value = await store.put(files[i]);
-      }
-      await tx.done;
-    }
-
-    return files || [];
   }
 );
 
@@ -102,13 +68,6 @@ export const slice = createSlice({
       .addCase(initial.fulfilled, (state, action) => {
         state.loading = false;
         state.list = action.payload || [];
-      })
-      .addCase(updateFiles.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(updateFiles.fulfilled, (state, action) => {
-        state.loading = false;
-        state.list = action.payload;
       });
   },
 });
