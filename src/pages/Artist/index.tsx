939525de61f229b5350948762af4d6a7cb7fee63
farbod <farboddeylami@gmail.com>
2022-02-10 20:08:29 +0330

    artist page can play track

--- src/pages/Artist/index.tsx
+++ src/pages/Artist/index.tsx
@@ -1,11 +1,15 @@
 import { useParams } from 'react-router-dom';
 import { openDB } from 'idb';
-import { Header, Sidebar, Loading, Image } from '../../components';
+import { Header, Image } from '../../components';
+import { useAppDispatch } from '../../hooks';
+import { selectedFileChange } from '../../slice';
 import { Item } from './components';
 import { useArtist } from './hooks';
 import styles from './styles.module.scss';
 
 const Artist = () => {
+  const dispatch = useAppDispatch();
+
   const { key = '', name } = useParams<'key' | 'name'>();
   const { info, list, loading } = useArtist(openDB, key);
 
@@ -35,8 +39,7 @@ const Artist = () => {
                 artist={item.artist}
                 cover={item.cover}
                 onSelect={() => {
-                  // navigate(`/albums/${item.key}/${item.name.replaceAll(' ', '-')}`);
-                  // dispatch(selectedKeyChange({ key: item.key }));
+                  dispatch(selectedFileChange({ key: item.key }));
                 }}
               />
             );
